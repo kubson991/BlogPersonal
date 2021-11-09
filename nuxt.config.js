a90c6c9482e705c7ab0947f93cfe49e092675121
SebastianPoveda <poveda1999@gmail.com>
2021-11-09 16:22:05 -0500

    a

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   components: true,
-  Target:'static',
+  target:'static',
   // Target: https://go.nuxtjs.dev/config-target
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
